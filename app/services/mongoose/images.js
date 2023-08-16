d93cf722fd54b93cca5b67612f8b62efca89b5c3
lcver <crackover1092@gmail.com>
2023-08-16 17:55:16 +0700

    [add] talent api

--- app/services/mongoose/images.js
+++ app/services/mongoose/images.js
@@ -1,19 +1,29 @@
-const Images = require('../../api/v1/images/model')
+const Images = require("../../api/v1/images/model");
+const { NotFoundError } = require("../../errors");
 
 const generateUrlImage = async (req) => {
     const result = `uploads/${req.file.filename}`;
 
     return result;
-}
+};
 
 const createImages = async (req) => {
     const result = await Images.create({
-        name: req.file 
-            ? `uploads/${req.file.filename}` 
-            : 'uploads/avatar/default.jpeg'
-    })
+        name: req.file
+            ? `uploads/${req.file.filename}`
+            : "uploads/avatar/default.jpeg",
+    });
 
     return result;
-}
+};
 
-module.exports = { createImages, generateUrlImage }
+const checkingImage = async (id) => {
+    const result = await Images.findOne({ _id: id });
+    console.log(result);
+
+    if (!result) throw new NotFoundError(`Tidak ada Gambar dengan id : ${id}`);
+
+    return result;
+};
+
+module.exports = { createImages, generateUrlImage, checkingImage };
